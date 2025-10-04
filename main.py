5c34808621b4a84767ff8e8324d03dd2c3f21ab4
bambooggxd-del <bambooggxd@gmail.com>
2025-10-04 15:06:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import re
 
 from server import server_on
 # ตั้งค่าเรท
-gamepass_rate = 6.2
+gamepass_rate = 6
 group_rate_low = 4
 group_rate_high = 4.5
 
@@ -843,4 +843,5 @@ async def od(ctx, *, expression: str):
 server_on()
 # เริ่มการทำงานบอท
 bot.run(os.getenv("TOKEN"))
+
 
