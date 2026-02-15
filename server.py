90dc45a7f09cc3ebeb2fdf2b56724abd8a879647
bambooggxd-del <bambooggxd@gmail.com>
2026-02-15 17:04:16 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,1 @@
-from flask import Flask
-import threading
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Sushi Shop Bot is running!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def server_on():
-    t = threading.Thread(target=run)
-    t.daemon = True
-    t.start()
-    print("✅ Server started on port 8080")
